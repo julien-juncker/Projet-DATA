f0f3c7b08ab57aed4f968845d7ec34e2fc2165e4
pierre-geeraert <pierre.geeraertviacesi.fr>
2019-07-02 11:59:53 +0200

    try to develop genetic algorithm

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,44 +1,17 @@
 import json
 
-
-data = {}
-data['people'] = []
-data['people'].append({
-    'name': 'Scott',
-    'website': 'stackabuse.com',
-    'from': 'Nebraska'
-})
-data['people'].append({
-    'name': 'Larry',
-    'website': 'google.com',
-    'from': 'Michigan'
-})
-data['people'].append({
-    'name': 'Tim',
-    'website': 'apple.com',
-    'from': 'Alabama'
-})
-
-with open('data.txt', 'w') as outfile:
-    json.dump(data, outfile)
-
-
-
-
-#mon_fichier = open("../data simulation/1000 villes/result_tronque.json","r")
-#contenu = mon_fichier.read()
-#print(contenu)
-
+#open and load json data
 with open("../data simulation/1000 villes/result_tronque.json") as json_file:
     data = json.load(json_file)
 
-
+#retrieve neighbours from json
 def get_neighbours(id):
     return data[id]['Neighbours']
 
-def get_neighbours_id(id):
-    return data[id]['Neighbours']['City']
+#retrieve city from neighbours from json NOT OPERATIONAL NOW
+def get_neighbours_id(id,nbr_item):
+    for i in range(0, nbr_item):
+        return data[0]['Neighbours'][i]["City"]
 
-#print(get_neighbours(2))
-#print(get_neighbours_id(2))
-print(data[0]['Neighbours']["City"])
+
+
