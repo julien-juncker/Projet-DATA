02ea1364c5ad286902c8d0ed3069ce2eb8b97671
Thomas DECLETY <thomas.declety@viacesi.fr>
2019-07-04 15:13:36 +0200

    update

--- GraphGenerator/GraphGenerator/Program.cs
+++ GraphGenerator/GraphGenerator/Program.cs
@@ -21,35 +21,60 @@ namespace GraphGenerator
             Stopwatch s = new Stopwatch();
             s.Start();
 
+            var temp = File.ReadAllLines(@"D:\drive\EXIA\A3\UE 5\projet data\git\Projet-DATA\VRP_python\test1\result.json");
+
+            List<mesure> re = new List<mesure>();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i] = temp[i].Replace('.', ',');
+                var tem = temp[i].Split(':');
+                int nb1 = 0;
+                int.TryParse(tem[0], out nb1);
+                float nb2 = 0;
+                float.TryParse(tem[2], out nb2);
+                int nb3 = 0;
+                int.TryParse(tem[2], out nb3);
+                re.Add(new mesure() {nbcities = nb1, time = nb2});
+            }
+
+            string sss = JsonConvert.SerializeObject(re);
+            
+
+
             List<City> graph = new List<City>();
             for (int i = 0; i < cities; i++)
             {
                 graph.Add(AddCity(graph, i));
             }
 
-            int[,] matrix = new int[cities, cities];
-
-            for (int i = 0; i < cities; i++)
+            using (StreamWriter sw = File.CreateText(@"D:\Documents\test.json"))
             {
-                for (int j = 0; j < cities; j++)
-                {
-                    matrix[i,j] =(j !=i) ?  GetDistance(graph[i], graph[j]) : 0;
-                    Console.Write(matrix[i, j] + "  ");
-                }
-                Console.WriteLine();
+                sw.WriteLine(JsonConvert.SerializeObject(graph, Formatting.Indented));
             }
 
-            string result = JsonConvert.SerializeObject(matrix, Formatting.Indented);
+            //int[,] matrix = new int[cities, cities];
 
-            string path = @"D:\Documents\Programmation\C#\GraphGenerator\GraphGenerator\result.json";
+            //for (int i = 0; i < cities; i++)
+            //{
+            //    for (int j = 0; j < cities; j++)
+            //    {
+            //        matrix[i,j] =(j !=i) ?  GetDistance(graph[i], graph[j]) : 0;
+            //        Console.Write(matrix[i, j] + "  ");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            using (StreamWriter sw = File.CreateText(path))
-            {
-                sw.WriteLine(result);
-            }
+            //string result = JsonConvert.SerializeObject(matrix, Formatting.Indented);
 
-            Console.Write(s.ElapsedMilliseconds);
-            Console.ReadLine();
+            //string path = @"D:\Documents\Programmation\C#\GraphGenerator\GraphGenerator\result.json";
+
+            //using (StreamWriter sw = File.CreateText(path))
+            //{
+            //    sw.WriteLine(result);
+            //}
+
+            //Console.Write(s.ElapsedMilliseconds);
+            //Console.ReadLine();
         }
 
         static City AddCity(List<City> graph, int id)
@@ -86,5 +111,22 @@ namespace GraphGenerator
         public int ID { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
+    }
+
+    class stat
+    {
+        public List<mesure> One { get; set; }
+        public List<mesure> Two { get; set; }
+        public List<mesure> Three { get; set; }
+        public List<mesure> Four { get; set; }
+        public List<mesure> Five { get; set; }
+        public List<mesure> Six { get; set; }
+    }
+    class mesure
+    {
+        public int distance { get; set; }
+        public int truck { get; set; }
+        public int nbcities { get; set; }
+        public float time { get; set; }
     }
 }
